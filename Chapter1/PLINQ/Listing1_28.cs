48e55de4b6b7c662116e5c77fea06b406e2c6df3
ashutosh.nigam <ashutosh.nigam@live.com>
2021-11-14 10:15:47 +0530

    Added Listing1_28.cs

---
+++ Chapter1/PLINQ/Listing1_28.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Chapter1.PLINQ
+{
+    public class Listing1_28 : IMain
+    {
+        public void Run()
+        {
+            BlockingCollection<string> col = new BlockingCollection<string>();
+            Task read = Task.Run(() =>
+            {
+                while (true)
+                    Console.WriteLine(col.Take());
+            });
+
+            Task write = Task.Run(() =>
+            {
+                while (true)
+                {
+                    string s = Console.ReadLine();
+                    if (string.IsNullOrEmpty(s)) break;
+                    col.Add(s);
+
+                }
+            });
+            write.Wait();
+        }
+    }
+}
--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -15,7 +15,7 @@ namespace Chapter1
         {
             try
             {
-                IMain m = new Listing1_27();
+                IMain m = new Listing1_28();
 
                 m.Run();
                 //var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
